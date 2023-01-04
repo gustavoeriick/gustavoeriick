519fcbe841675635c058323c5280ea9459f7e5c5
Gustavo Ferreira <gustavo.eriick@hotmail.com>
2023-01-04 15:43:46 -0300

    Array + Loop - 04012023

--- carrossel_vertical/js/dados-titular.js
+++ carrossel_vertical/js/dados-titular.js
@@ -1,5 +1,67 @@
 $(document).ready(function(){
-    $.getJSON('/outros/dados.json',function(v){
-        $('#dados-titular').html('<div class="carteirinha"><div class="info-cliente p-cobertura"><span class="info-titulo">Plano de cobertura</span><br><span class="info-resultado">'+v.plano+'</span></div><div class="info-cliente n-carteira"><span class="info-titulo">Número da carteira</span><br><span class="info-resultado numeroc">'+v.numeroc+'</span></div><br><div class="w-100 info-cliente"><span class="info-titulo">Usuário</span><br><span class="info-resultado">'+v.usuario+'</span></div><br><div class="w-100 info-cliente"><span class="info-titulo">Titular</span><br><span class="info-resultado">'+v.titular+'</span></div><br><div class="w-100 info-cliente"><span class="info-titulo">Empresa</span><br><span class="info-resultado">'+v.empresa+'</span></div><br><div class="w-100 info-cliente info">Ao marcar sua consulta, informe que você utiliza a Rede Sempre Odonto</div><br><div class="div-linha"></div><br><div class="info-outras"><span>Serviço de Atendimento ao Cliente:</span><br><span>SP:(11)4861.2434</span><br><span>Demais localidades: 3003.9420</span><br><span>www.sempreodonto.com.br</span></div><div class="ans text-end"><img src = "imagens/ans.png" class = "icone-ans"></div></div>');
+    $.getJSON('/outros/dados.json',function(retorno){
+
+        var string_html = "";
+        string_html = string_html + '    <div class="carousel-item active" data-bs-interval="1000000">';
+        string_html = string_html + '      <img src="imagens/carteirinha.png" class="img-carrossel" alt="...">';
+        string_html = string_html + '     </div>';
+
+        for (var v in retorno)
+        {
+            string_html = string_html + '      <div class="carousel-item" data-bs-interval="1000000">';
+            string_html = string_html + '<div class="carteirinha">';
+            string_html = string_html + '<div class="info-cliente p-cobertura">';
+            string_html = string_html + '    <span class="info-titulo">Plano de cobertura</span>';
+            string_html = string_html + '    <br>';
+            string_html = string_html + '    <span class="info-resultado">'+retorno[v].plano+'</span>';
+            string_html = string_html + '</div>';
+            string_html = string_html + '<div class="info-cliente n-carteira">';
+            string_html = string_html + '    <span class="info-titulo">Número da carteira</span>';
+            string_html = string_html + '    <br>';
+            string_html = string_html + '    <span class="info-resultado numeroc">'+retorno[v].numeroc+'</span>';
+            string_html = string_html + '</div>';
+            string_html = string_html + '<br>';
+            string_html = string_html + '<div class="w-100 info-cliente">';
+            string_html = string_html + '    <span class="info-titulo">Usuário</span>';
+            string_html = string_html + '    <br>';
+            string_html = string_html + '    <span class="info-resultado">'+retorno[v].usuario+'</span>';
+            string_html = string_html + '</div>';
+            string_html = string_html + '<br>';
+            string_html = string_html + '<div class="w-100 info-cliente">';
+            string_html = string_html + '    <span class="info-titulo">Titular</span>';
+            string_html = string_html + '    <br>';
+            string_html = string_html + '    <span class="info-resultado">'+retorno[v].titular+'</span>';
+            string_html = string_html + '</div>';
+            string_html = string_html + '<br>';
+            string_html = string_html + '<div class="w-100 info-cliente">';
+            string_html = string_html + '    <span class="info-titulo">Empresa</span>';
+            string_html = string_html + '    <br>';
+            string_html = string_html + '    <span class="info-resultado">'+retorno[v].empresa+'</span>';
+            string_html = string_html + '</div>';
+            string_html = string_html + '<br>';
+            string_html = string_html + '<div class="w-100 info-cliente info">';
+            string_html = string_html + '    Ao marcar sua consulta, informe que você utiliza a Rede Sempre Odonto';
+            string_html = string_html + '</div>';
+            string_html = string_html + '<br>';
+            string_html = string_html + '<div class="div-linha"></div>';
+            string_html = string_html + '<br>';
+            string_html = string_html + '<div class="info-outras">';
+            string_html = string_html + '    <span>Serviço de Atendimento ao Cliente:</span>';
+            string_html = string_html + '    <br>';
+            string_html = string_html + '    <span>SP:(11)4861.2434</span>';
+            string_html = string_html + '    <br>';
+            string_html = string_html + '    <span>Demais localidades: 3003.9420</span>';
+            string_html = string_html + '    <br>';
+            string_html = string_html + '    <span>www.sempreodonto.com.br</span>';
+            string_html = string_html + '</div>';
+            string_html = string_html + '<div class="ans text-end">';
+            string_html = string_html + '    <img src = "imagens/ans.png" class = "icone-ans">';
+            string_html = string_html + '</div>';
+            string_html = string_html + '</div>';
+            string_html = string_html + '</div>';
+        }
+
+
+        $('.carousel-inner').html(string_html);
     });
 });
