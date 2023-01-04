342c83e8e8521130cc70c2323ae970fcca6d85e0
Gustavo Ferreira <gustavo.eriick@hotmail.com>
2023-01-04 14:11:05 -0300

    usando jquery + json + html no carrossel vertical - 04012023

---
+++ carrossel_vertical/js/dados-titular-teste.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#driver").click(function(){
+        $.getJSON('/outros/dados.json',function(v){
+            $('#stage').html('<div class="carteirinha"><div class="info-cliente p-cobertura"><span class="info-titulo">Plano de cobertura</span><br><span class="info-resultado">'+v.plano+'</span></div><div class="info-cliente n-carteira"><span class="info-titulo">Número da carteira</span><br><span class="info-resultado n-carteirinha">'+v.ncarteirinha+'</span></div><br><div class="w-100 info-cliente"><span class="info-titulo">Usuário</span><br><span class="info-resultado">'+v.usuario+'</span></div><br><div class="w-100 info-cliente"><span class="info-titulo">Titular</span><br><span class="info-resultado">'+v.titular+'</span></div><br><div class="w-100 info-cliente"><span class="info-titulo">Empresa</span><br><span class="info-resultado">'+v.empresa+'</span></div><br><div class="w-100 info-cliente info">Ao marcar sua consulta, informe que você utiliza a Rede Sempre Odonto</div><br><div class="div-linha"></div><br><div class="info-outras"><span>Serviço de Atencimento ao Cliente:</span><br><span>SP:(11)4861.2434</span><br><span>Demais localidades: 3003.9420</span><br><span>www.sempreodonto.com.br</span></div><div class="ans text-end"><img src = "imagens/ans.png" class = "icone-ans"></div></div>');
+        });
+    });
+});
